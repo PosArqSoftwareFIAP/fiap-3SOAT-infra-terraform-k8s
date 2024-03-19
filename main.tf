bdb8dc47b0b749574c5d3cdcf46eec04785ad6ec
Leonilson Alencar <86755623+Ralrargrun@users.noreply.github.com>
2024-03-19 18:33:53 -0300

    up

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
+provider "digitalocean" {
+  token = local.do_token
 }
 
-data "aws_vpc" "default" {
+resource "digitalocean_kubernetes_cluster" "my_k8s_cluster" {
+  name    = "fiap-food-k8"
+  region  = "nyc1"
+  version = "1.29.1-do.0"
+
+  node_pool {
+    name       = "fiap-food-pool"
+    size       = "s-1vcpu-1gb"
+    node_count = 1
+  }
 }
