ccc519a39876d5cfad673e5f26703c2dac69abc1
Leonilson Alencar <86755623+Ralrargrun@users.noreply.github.com>
2024-03-19 18:46:22 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.0.0" # specify your required version
+    }
+  }
+}
+
 provider "digitalocean" {
   token = local.do_token
 }
